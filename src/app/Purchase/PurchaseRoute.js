431189759dfb77d8c759a687f26e91a1951ef2fd
Tam Pham <tampham47@live.com>
2021-10-31 13:43:29 +0700

    create verify and distribute function

--- src/app/Purchase/PurchaseRoute.js
+++ src/app/Purchase/PurchaseRoute.js
@@ -3,6 +3,8 @@ import PurchaseController from "./PurchaseController";
 
 const router = Router();
 
+router.post("/verify", PurchaseController.verify);
 router.post("/submit", PurchaseController.submit);
+router.post("/distribute", PurchaseController.distribute);
 
 export default router;
